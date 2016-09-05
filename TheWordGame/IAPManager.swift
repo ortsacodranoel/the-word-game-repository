d9a6a555f6ad9104d4154952ef345778825d3e28
Leo Castro <ortsacodranoel@gmail.com>
2016-09-05 16:41:50 -0400

    Added IAPManager

---
+++ TheWordGame/IAPManager.swift
@@ -0,0 +1,55 @@
+//
+//  IAPManager.swift
+//  testing-animations
+//
+//  Created by Leo on 9/5/16.
+//  Copyright © 2016 Daniel Castro. All rights reserved.
+//
+
+import Foundation
+import StoreKit
+
+class IAPManager: NSObject, SKProductsRequestDelegate {
+    
+    static let sharedInstance = IAPManager()
+    
+    var request:SKProductsRequest!
+    var products:NSArray!
+    
+    
+    func setupInAppPurchases() {
+        self.validateProductIdentifiers(self.getProductIdentifiersFromMainBundle())
+    }
+    
+    
+    func getProductIdentifiersFromMainBundle() ->NSArray {
+        
+        var identifiers = NSArray()
+        
+        // This will give us an array of product identifiers.
+        if let url = NSBundle.mainBundle().URLForResource("iap_products", withExtension: "plist") {
+            identifiers = NSArray(contentsOfURL: url)!
+        }
+        
+        return identifiers
+        
+    }
+    
+    
+    func validateProductIdentifiers(identifiers:NSArray) {
+        
+        let productIdentifiers = NSSet(array: identifiers as [AnyObject])
+        
+        let productRequest = SKProductsRequest(productIdentifiers: productIdentifiers as! Set<String>)
+        
+        self.request = productRequest
+        productRequest.start()
+    }
+    
+    
+    func productsRequest(request:SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
+        self.products = response.products
+        print(self.products)
+    }
+    
+}
