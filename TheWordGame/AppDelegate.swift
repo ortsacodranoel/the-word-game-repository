480faea1877ecdc7904283376622a7d0573f0aa5
Leo Castro <ortsacodranoel@gmail.com>
2016-09-11 23:39:34 -0400

    Added two categories.

--- TheWordGame/AppDelegate.swift
+++ TheWordGame/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             canPurchase = true
             IAPManager.sharedInstance.setupInAppPurchases()
         }
-        
+    
         IAPManager.sharedInstance.restorePurchases()
         
         return true
