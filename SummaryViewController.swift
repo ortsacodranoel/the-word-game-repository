f8d80d218ad2e01fc82a77c4eaba4270f5a90d18
Leo Castro <ortsacodranoel@gmail.com>
2016-10-01 17:58:06 -0400

    wordSummaryTextview fix
    - textview starting at top

--- SummaryViewController.swift
+++ SummaryViewController.swift
@@ -38,64 +38,59 @@ class SummaryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let words = NSMutableAttributedString()
         
+        // Used to append correct/missed words.
+        let words = NSMutableAttributedString()
         // Used to add the carriage return to the attributed string passed to the wordSummaryTextview.
         let cr = NSMutableAttributedString(string: "\n")
-        
         // Retrieve the missed words.
         for missedWord in Game.sharedGameInstance.missedWordsArray {
-            
             // Used as temp storage for missed words.
             let myString = NSMutableAttributedString (string: missedWord, attributes: [NSFontAttributeName:UIFont(name: "Helvetica Neue", size: 25.0)!])
-            
             // Used to color the missed words red.
             myString.addAttribute(NSForegroundColorAttributeName, value: UIColor.red, range: NSRange(location:0,length:myString.length))
             words.append(myString)
-            
             // Append carriage return.
             words.append(cr)
-            
             self.wordSummaryTextview.attributedText = words
+            
         }
-        
         // Retrieve correct words.
         for correctWord in Game.sharedGameInstance.correctWordsArray {
-            
             // Used as temp storage for missed words.
             let myString = NSMutableAttributedString (string: correctWord, attributes: [NSFontAttributeName:UIFont(name: "Helvetica Neue", size: 25.0)!])
-            
             // Used to color the missed words green.
             myString.addAttribute(NSForegroundColorAttributeName, value: UIColor.green, range: NSRange(location:0,length:myString.length))
             words.append(myString)
-            
             // Append carriage return.
             words.append(cr)
-            
             self.wordSummaryTextview.attributedText = words
-            
         }
+        
 
-      self.wordSummaryTextview.scrollRangeToVisible(NSMakeRange(0,0))
-        
-        
-      //  self.wordSummaryTextview.setContentOffset(CGPoint(x:0, y:0), animated: true)
-                self.wordSummaryTextview.textAlignment = .center
+        self.wordSummaryTextview.textAlignment = .center
+    
     }
     
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.wordSummaryTextview.setContentOffset(CGPoint.zero, animated: false)
+    }
+    
+    
+    /// Method:
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    
+    /// Method:
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-
-        
-        
-        
         
         // Set the color of the category.
         let categoryColor = Game.sharedGameInstance.gameColor.cgColor
@@ -104,18 +99,17 @@ class SummaryViewController: UIViewController {
         self.mainView.layer.borderColor = categoryColor
         self.mainView.layer.borderWidth = 3
         self.backgroundView.layer.cornerRadius = 7
-        
         self.wordSummaryLabel.textColor = Game.sharedGameInstance.gameColor
+
         
         let underlineAttribute = [NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue]
         let underlineAttributedString = NSAttributedString(string: "Words Summary", attributes: underlineAttribute)
- 
         self.wordSummaryLabel.attributedText = underlineAttributedString
-        
-        
         
 
         
+        
+
     }
     
     
