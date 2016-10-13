ed8e137ca49749b2d3748cf9f9b0bdaf92168ac4
Leo Castro <ortsacodranoel@gmail.com>
2016-10-13 19:51:09 -0400

    Add SKPaymentQueue to the AppDelegate
    added:
    
            if SKPaymentQueue.canMakePayments(){
    
                canPurchase = true
                IAPManager.sharedInstance.setupInAppPurchases()
            }
    
            IAPManager.sharedInstance.restorePurchases()

--- TheWordGame/AppDelegate.swift
+++ TheWordGame/AppDelegate.swift
@@ -24,6 +24,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
     
+        
+        if SKPaymentQueue.canMakePayments(){
+            
+            canPurchase = true
+            IAPManager.sharedInstance.setupInAppPurchases()
+        }
+        
+        IAPManager.sharedInstance.restorePurchases()
+        
+        
+        
+        
+        
         let fetchRequest : NSFetchRequest<TutorialPopUp>
         // 1. Create the fetch request for all entities of type TutorialPopUp.
         
