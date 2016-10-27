daa3921870cba81537ea6f1c97694d5578c89e95
Daniel Castro <ortsacodranoel@gmail.com>
2016-10-27 11:32:12 -0400

    Change font size in summary

--- SummaryViewController.swift
+++ SummaryViewController.swift
@@ -14,119 +14,49 @@ import CoreData
 
 class SummaryViewController: UIViewController {
 
-    // MARK: - General properties
     var categoryTapped = Int()
-    
-    // MARK: - Views
     @IBOutlet weak var mainView: UIView!
     @IBOutlet weak var backgroundView: UIView!
-    
-    
-    // MARK: - Label
     @IBOutlet weak var wordSummaryLabel: UILabel!
-    
     @IBOutlet weak var wordSummaryTextview: UITextView!
-    
-    
 
     
-    
-    
-    // MARK: - Sound effect paths.
-    
-    // Used to create tap sound effect when button touched.
-    let soundEffectButtonTap = URL(fileURLWithPath: Bundle.main.path(forResource: "ButtonTapped", ofType: "wav")!)
-    // Used for menu interactions sounds.
-    var audioPlayerButtonTapSound = AVAudioPlayer()
-    
-    
-    
-    
-    // MARK: - BUTTON ACTIONS
 
-    @IBAction func backBtnTapped(_ sender: AnyObject) {
-        print("Back button tapped")
-    }
-    
-    
-    
-    
-    // MARK: - AUDIO SETUP
-    
-    /// Configures the AVAudioPlayers with their respective sound files and prepares them to be played.
-    func loadSounds() {
-        do {
-            // Configure Audioplayers.
-            self.audioPlayerButtonTapSound = try AVAudioPlayer(contentsOf: self.soundEffectButtonTap, fileTypeHint: "wav")
-            // Prepare to play.
-            self.audioPlayerButtonTapSound.prepareToPlay()
-        } catch {
-            print("Error: unable to find sound files.")
-        }
-    }
-    
-    
-    
-    
-    
-    // MARK: - VIEW SETUP
-    
-
+    // MARK: - View Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Used to append correct/missed words.
         let words = NSMutableAttributedString()
-        // Used to add the carriage return to the attributed string passed to the wordSummaryTextview.
         let cr = NSMutableAttributedString(string: "\n")
-        
         
         // Retrieve the missed words.
         for missedWord in Game.sharedGameInstance.missedWordsArray {
-          
-            let attrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 30)]
-
+            let attrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 23)]
             let myString = NSMutableAttributedString(string: missedWord, attributes:attrs)
             myString.addAttribute(NSForegroundColorAttributeName, value: UIColor.red, range: NSRange(location:0,length:myString.length))
             
-            // Add the new word to the string.
             words.append(myString)
-            // Append carriage return.
             words.append(cr)
-            // Add word to text view.
             self.wordSummaryTextview.attributedText = words
-            
         }
         // Retrieve correct words.
         for correctWord in Game.sharedGameInstance.correctWordsArray {
             
-            let attrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 30)]
+            let attrs = [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 23)]
             
             let myString = NSMutableAttributedString(string: correctWord, attributes:attrs)
             myString.addAttribute(NSForegroundColorAttributeName, value: UIColor.green, range: NSRange(location:0,length:myString.length))
           
-            // Add the new word to the string.
             words.append(myString)
-            // Append carriage return.
             words.append(cr)
             self.wordSummaryTextview.attributedText = words
         }
-        
-
+    
         self.wordSummaryTextview.textAlignment = .center
-        
-        
-        
-        
-        // Load sounds.
-        self.loadSounds()
     }
     
-    
-    /*
-     Used to layout the wordSummaryTextview when the screen noted.
-    */
+
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         self.wordSummaryTextview.setContentOffset(CGPoint.zero, animated: false)
@@ -142,25 +72,25 @@ class SummaryViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Set the color of the category.
         let categoryColor = Game.sharedGameInstance.gameColor.cgColor
-        // Setup view styles.
+
         self.mainView.layer.cornerRadius = 7
         self.mainView.layer.borderColor = categoryColor
         self.mainView.layer.borderWidth = 3
         self.backgroundView.layer.cornerRadius = 7
         self.wordSummaryLabel.textColor = Game.sharedGameInstance.gameColor
-
         
         let underlineAttribute = [NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue]
         let underlineAttributedString = NSAttributedString(string: "Words Summary", attributes: underlineAttribute)
         self.wordSummaryLabel.attributedText = underlineAttributedString
-
     }
     
     
-
-    /// Set the initial background color of the main view.
+    // MARK: - Button Actions
+    
+    @IBAction func backBtnTapped(_ sender: AnyObject) {
+    }
+        
     func setColorForViewBackground() {
         self.view.backgroundColor = Game.sharedGameInstance.colors[categoryTapped]
     }
