bc81fa27d673048bd63231813b70b5d27ec2f20b
Daniel Castro <ortsacodranoel@gmail.com>
2016-10-29 15:18:40 -0400

    TransitionManager comments update

--- TheWordGame/RulesTransitionManager.swift
+++ TheWordGame/RulesTransitionManager.swift
@@ -1,5 +1,5 @@
 //
-//  TransitionManager.swift
+//  RulesTransitionManager.swift
 //  TheWordGame
 //
 //  Created by Daniel Castro on 6/23/16.
@@ -11,15 +11,10 @@ import UIKit
 
 class RulesTransitionManager: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate  {
     
-    // MARK: UIViewControllerAnimatedTransitioning protocol methods
     fileprivate var presenting = true
-    
-    // MARK: Information passed.
     var CategoryTapped = Int()
     
-    // Animate transition between View Controllers.
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        
         let container = transitionContext.containerView
         let categoriesView = transitionContext.view(forKey: UITransitionContextViewKey.from)!
         let rulesView = transitionContext.view(forKey: UITransitionContextViewKey.to)!
@@ -52,23 +47,15 @@ class RulesTransitionManager: NSObject, UIViewControllerAnimatedTransitioning, U
         })
     }
     
-    
-    // return how many seconds the transiton animation will take
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return 0.5
     }
-    
-    // MARK: UIViewControllerTransitioningDelegate protocol methods
-    
-    // return the animataor when presenting a viewcontroller
-    // remmeber that an animator (or animation controller) is any object that aheres to the UIViewControllerAnimatedTransitioning protocol
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         self.presenting = true
         return self
     }
     
-    // return the animator used when dismissing from a viewcontroller
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         self.presenting = false
         return self
